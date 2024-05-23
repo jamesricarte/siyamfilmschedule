96e73d2f279e2450091aaa3cc78fab99090ba90a
jamesricarte <126384275+jamesricarte@users.noreply.github.com>
2024-05-23 19:31:31 +0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,120 @@
+function dropdownbutton1Clicked() {
+    const Script = document.querySelector('.js-script-1');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-1').addEventListener('click', dropdownbutton1Clicked);
+
+function dropdownbutton2Clicked() {
+    const Script = document.querySelector('.js-script-2');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-2').addEventListener('click', dropdownbutton2Clicked);
+
+function dropdownbutton3Clicked() {
+    const Script = document.querySelector('.js-script-3');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-3').addEventListener('click', dropdownbutton3Clicked);
+
+function dropdownbutton4Clicked() {
+    const Script = document.querySelector('.js-script-4');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-4').addEventListener('click', dropdownbutton4Clicked);
+
+function dropdownbutton5Clicked() {
+    const Script = document.querySelector('.js-script-5');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-5').addEventListener('click', dropdownbutton5Clicked);
+
+function dropdownbutton6Clicked() {
+    const Script = document.querySelector('.js-script-6');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-6').addEventListener('click', dropdownbutton6Clicked);
+
+function dropdownbutton7Clicked() {
+    const Script = document.querySelector('.js-script-7');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-7').addEventListener('click', dropdownbutton7Clicked);
+
+function dropdownbutton8Clicked() {
+    const Script = document.querySelector('.js-script-8');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-8').addEventListener('click', dropdownbutton8Clicked);
+
+function dropdownbutton9Clicked() {
+    const Script = document.querySelector('.js-script-9');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-9').addEventListener('click', dropdownbutton9Clicked);
+
+function dropdownbutton10Clicked() {
+    const Script = document.querySelector('.js-script-10');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-10').addEventListener('click', dropdownbutton10Clicked);
+
+function dropdownbutton11Clicked() {
+    const Script = document.querySelector('.js-script-11');
+    if (Script.style.display === 'none' || Script.style.display === '') {
+        Script.style.display = 'flex';
+    } else {
+        Script.style.display = 'none';
+    }
+}
+
+document.querySelector('.js-button-11').addEventListener('click', dropdownbutton11Clicked);
